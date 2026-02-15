0afb9550728ee122def5b86d13f2d43906e5d390
LoneWolf132 <sreyesaravinds@gmail.com>
2026-02-15 18:49:23 +0530

    Edited: No need to create cleaned dataset again

--- lr_model.py
+++ lr_model.py
@@ -6,16 +6,16 @@ from sklearn.linear_model import LogisticRegression
 import os
 os.system('cls')
 cwd=os.path.dirname(os.path.abspath(__file__))
-dataset_path= os.path.join(cwd, "depression_dataset.csv")
+dataset_path= os.path.join(cwd, "depression_dataset_cleaned.csv")
 
 #pandas dataframe
 df = pd.read_csv(dataset_path)
 
-df_cleaned = df[df["Sleep Duration"] != 'Others'] 
+'''df_cleaned = df[df["Sleep Duration"] != 'Others'] 
 df_cleaned = df_cleaned[df_cleaned['Dietary Habits'] != 'Others'] 
 #'Dietary Habits'
 df_cleaned = df_cleaned.drop(columns=["Work Pressure"])
 df_cleaned = df_cleaned.drop(columns=["Job Satisfaction"])
 print(df.shape,df_cleaned.shape,sep='\n')
 cleaned_dataset_path = os.path.join(cwd, "depression_dataset_cleaned.csv")
-df_cleaned.to_csv(cleaned_dataset_path, index=False)
+df_cleaned.to_csv(cleaned_dataset_path, index=False)'''
