f952f5b1a8d18e502f16ecb64a06c553fec57f31
LoneWolf132 <sreyesaravinds@gmail.com>
2026-02-15 18:54:01 +0530

    Edited: Mapped non-numerical data in dataset to appropriate indices

--- lr_model.py
+++ lr_model.py
@@ -19,3 +19,29 @@ df_cleaned = df_cleaned.drop(columns=["Job Satisfaction"])
 print(df.shape,df_cleaned.shape,sep='\n')
 cleaned_dataset_path = os.path.join(cwd, "depression_dataset_cleaned.csv")
 df_cleaned.to_csv(cleaned_dataset_path, index=False)'''
+
+#convert graded values to numerical scales
+sleep_mapping = {
+    "Less than 5 hours": 1,   # very poor
+    "5-6 hours": 2,           # below ideal
+    "7-8 hours": 3,           # optimal
+    "More than 8 hours": 2   # can indicate oversleeping
+}
+
+diet_mapping = {
+    "Unhealthy": 1,
+    "Moderate": 2,
+    "Healthy": 3
+}
+
+yes_no_mapping = {
+    "No": 0,
+    "Yes": 1
+}
+
+df["Sleep Duration"] = df["Sleep Duration"].map(sleep_mapping)
+df["Dietary Habits"] = df["Dietary Habits"].map(diet_mapping)
+df["Have you ever had suicidal thoughts ?"] = df["Have you ever had suicidal thoughts ?"].map(yes_no_mapping)
+df["Family History of Mental Illness"] = df["Family History of Mental Illness"].map(yes_no_mapping)
+
+#print(df.head())
