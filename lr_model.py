92fd4e7947b5c1e34d756ecb70c977763cdc9ef1
LoneWolf132 <sreyesaravinds@gmail.com>
2026-02-16 21:23:47 +0530

    Edited: lr_model, throws NaN ValueError to be fixed

--- lr_model.py
+++ lr_model.py
@@ -3,7 +3,10 @@ import numpy as np
 import pandas as pd
 #from patsy import dmatrices
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.model_selection import train_test_split
 import os
+
 os.system('cls')
 cwd=os.path.dirname(os.path.abspath(__file__))
 dataset_path= os.path.join(cwd, "depression_dataset_cleaned.csv")
@@ -28,6 +31,11 @@ sleep_mapping = {
     "More than 8 hours": 2   # can indicate oversleeping
 }
 
+gender_mapping = {
+    "Male": 1,
+    "Female": 0
+}
+
 diet_mapping = {
     "Unhealthy": 1,
     "Moderate": 2,
@@ -38,10 +46,29 @@ yes_no_mapping = {
     "No": 0,
     "Yes": 1
 }
-
+del_cols=["id","City","Profession","Degree"]  #don't impact
 df["Sleep Duration"] = df["Sleep Duration"].map(sleep_mapping)
 df["Dietary Habits"] = df["Dietary Habits"].map(diet_mapping)
 df["Have you ever had suicidal thoughts ?"] = df["Have you ever had suicidal thoughts ?"].map(yes_no_mapping)
 df["Family History of Mental Illness"] = df["Family History of Mental Illness"].map(yes_no_mapping)
+df['Gender']=df['Gender'].map(gender_mapping)
+df.drop(columns=del_cols,inplace=True) #insignificant and redundant columns removed
 
-#print(df.head())
+#print(df.shape)
+#print(df.info())
+new_cols=["gender", "age", "academic_pressure","cgpa", "study_satisfaction", "sleep_duration","dietary_habits","suicidal_thoughts",
+"work_study_hours","financial_stress","family_history","depression"]
+df.columns=new_cols
+#print(df.info())
+
+#dependent and independent  feature sets 
+X=df.drop(columns='depression').copy()
+y=df['depression'].copy() 
+
+#train-test split
+X_train, X_test, y_train, y_test =train_test_split(X,y,test_size=0.2,random_state=42) 
+
+#logistic regression obj
+log_regression=LogisticRegression()
+print(X_train)
+log_regression.fit(X_train, y_train)
