379023eb609e659a6289d417c4a05a905f87ebab
LoneWolf132 <sreyesaravinds@gmail.com>
2026-02-20 13:25:22 +0530

    Edited: created a class for trigger words, vectorization train_x and train_y defined

--- lr_model_and_text_box_integration.py
+++ lr_model_and_text_box_integration.py
@@ -2,8 +2,18 @@
 import os
 import joblib
 import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
 os.system('cls')
-
+#importing model into the framework
 cwd=os.path.dirname(os.path.abspath(__file__))
 model_path= os.path.join(cwd, 'logistic_depression_model_revised.joblib')
-model = joblib.load(model_path)
+model = joblib.load(model_path) #let's leave it aside for time being, let's focus on nlp for now
+
+#tw identification - vectorization
+class trig_word:
+    FLD={'feel like dying', 'done with life', 'don\'t wanna live','suicide','kill myself'}
+    DEATH={'death','goodbye','bye'}
+    REGRET={'sorry','i\'ve caused so much pain'}
+
+train_X=["i feel like dying",'I am fed up with life', "i\'ve been a burden to everyone",'bye, maybe we shall meeet again in the afterlife']
+train_y=[trig_word.FLD,trig_word.FLD,trig_word.REGRET,trig_word.DEATH]
