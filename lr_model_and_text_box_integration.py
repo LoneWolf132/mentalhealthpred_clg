7b87655dc639a62d4a66afe2d6656d1a147925d5
LoneWolf132 <sreyesaravinds@gmail.com>
2026-02-25 22:07:59 +0530

    Edited: Trigger word mechanism

--- lr_model_and_text_box_integration.py
+++ lr_model_and_text_box_integration.py
@@ -11,21 +11,28 @@ cwd=os.path.dirname(os.path.abspath(__file__))
 model_path= os.path.join(cwd, 'logistic_depression_model_revised.joblib')
 model = joblib.load(model_path) #let's leave it aside for time being, let's focus on nlp for now
 
-#tw identification - vectorization
+
+# tw identification - vectorization
 class trig_word:
-    FLD={'feel like dying', 'done with life', 'don\'t wanna live','suicide','kill myself'}
-    DEATH={'death','goodbye','bye'}
-    REGRET={'sorry','i\'ve caused so much pain'}
+    FLD = {'feel like dying', 'done with life', 'don\'t wanna live','suicide','kill myself'}
+    DEATH = {'death','goodbye','bye'}
+    REGRET = {'sorry','i\'ve caused so much pain'}
 
-train_X=["i feel like dying",'I am fed up with life', "i\'ve been a burden to everyone",'bye, maybe we shall meet again in the afterlife']
-train_y=[trig_word.FLD,trig_word.FLD,trig_word.REGRET,trig_word.DEATH]
+train_X = [
+    "i feel like dying",
+    "I am fed up with life",
+    "i've been a burden to everyone",
+    "bye, maybe we shall meet again in the afterlife"
+]
 
-vectorizer_object=CountVectorizer(binary=True,ngram_range=(1,3))
+# FIXED LABELS (use class names, not sets)
+train_y = ["FLD", "FLD", "REGRET", "DEATH"]
+
+vectorizer_object = CountVectorizer(binary=True, ngram_range=(1,3))
 
 train_x_vectors = vectorizer_object.fit_transform(train_X)
 
 print(vectorizer_object.get_feature_names_out())
-
 print(train_x_vectors.toarray())
 
-#print(sys.executable)
+#up next - word embeddings
